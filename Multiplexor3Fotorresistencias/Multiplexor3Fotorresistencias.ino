4e409c85402c3ae4bbe476e2b4c2502cb3ce3970
Juan Eduardo Calderón Servín <juanixgh@gmail.com>
2016-08-02 23:27:23 -0500

    Se agregó código para el bluetooth y para multiplexor para tres fotorresistencias

---
+++ Multiplexor3Fotorresistencias/Multiplexor3Fotorresistencias.ino
@@ -0,0 +1,36 @@
+
+int64_t wait=1000;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);
+  DDRB =0b00000111;
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+
+  PORTB=0b00000000;
+  uint8_t Sensor0=analogRead(A0);
+  Serial.print("sensor0 ");
+  Serial.println(Sensor0);
+  delay(wait);
+
+  PORTB=0b00000001;
+  uint8_t Sensor1=analogRead(A0);
+  Serial.print("sensor1 ");
+  Serial.println(Sensor1);
+  delay(wait);
+
+  PORTB=0b00000010;
+  uint8_t Sensor2=analogRead(A0);
+  Serial.print("sensor2 ");
+  Serial.println(Sensor2);
+  delay(wait);
+
+  digitalWrite(7, HIGH);
+  delay(wait);
+  digitalWrite(7, LOW);
+}
---
+++ Para bluetooths/SegundaOpcion/SegundaOpcion.ino
@@ -0,0 +1,55 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(4, 2); // RX, TX
+int led=0;
+void setup()
+{
+// Open serial communications and wait for port to open:
+Serial.begin(9600);
+
+Serial.println("Conectado al Bluetooth!");
+// set the data rate for the SoftwareSerial port
+mySerial.begin(9600);
+mySerial.println("Conectado a la pc");
+pinMode(9,OUTPUT); //Pin para pruebas
+
+}
+void loop() // run over and over
+{
+  if(led==1)
+    digitalWrite(9,HIGH);
+    else{
+      if(led==0)
+    digitalWrite(9,LOW);
+    }
+if (mySerial.available()){
+//Aquí se reciben los datos del teléfono
+//Serial.write(mySerial.read());
+char C=mySerial.read();
+if(C=='A'){
+led=1;
+}
+else{
+  if(C=='a'){
+led=0;
+}
+
+}
+}
+
+if (Serial.available())
+//Aquí se envían los datos al celular
+mySerial.write(Serial.read());
+}
+
+String GetLine()
+   {   String S = "" ;
+       if (mySerial.available())
+          {    char c = mySerial.read(); ;
+                while ( c != '\n')            //Hasta que el caracter sea intro
+                  {     S = S + c ;
+                        delay(25) ;
+                        c = mySerial.read();
+                  }
+                return( S ) ;
+          }
+   }
